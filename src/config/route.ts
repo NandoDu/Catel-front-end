c3d3339c2f417ca1f6f83af85b9d849336b4e310
Turmiht <g13395930368@gmail.com>
2022-01-03 14:50:07 +0800

    fix: prevent re commit

--- src/config/route.ts
+++ src/config/route.ts
@@ -1,15 +1,14 @@
 import Login from '../page/Login.vue';
-import Register from '../page/Register.vue';
 import Home from '../page/Home.vue';
 import OrderDetail from '../page/OrderDetail.vue';
 import Info from '../page/HotelInfo.vue';
-import hotelInfo from '../component/HotelInfo/hotelInfo.vue';
 import PersonalInfo from '../page/PersonalInfo.vue';
 import Hotels from '../page/Hotels.vue';
 import OrderDetailPage from '../page/OrderDetailPage.vue';
 import AboutUs from '../page/AboutUs.vue';
 import {NavigationGuard, RouteRecordRaw} from 'vue-router';
 import {store} from '../store';
+import {ElMessage} from 'element-plus';
 
 const allowNotLogged: NavigationGuard = (to, from) => {
   const ok = !store.getters['user/logged'];
@@ -24,9 +23,19 @@ const allowLogged: NavigationGuard = (to, from) => {
   if (!ok) return {path: '/login', query: {redirect: to.path}};
 };
 
+const preventRecommit: NavigationGuard = (to, from) => {
+  const recommit = from.matched[0].path === '/order-detail/:orderId';
+  if (!recommit) return;
+  ElMessage.warning({
+    message: '订单已提交，可在个人中心内查看',
+    center: true,
+  });
+  return '/';
+};
+
 const routes: RouteRecordRaw[] = [
   {path: '/', component: Home},
-  {path: '/order', component: OrderDetail},
+  {path: '/order', component: OrderDetail, beforeEnter: preventRecommit},
   {path: '/hotels', component: Hotels},
   {path: '/login', component: Login, beforeEnter: allowNotLogged},
   {path: '/hotel/:id', component: Info},
